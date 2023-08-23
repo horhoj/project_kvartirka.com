61965a89f59cc824b16277b2da456b280e854d40
Василий Задоя <cool29@yandex.ru>
2023-08-24 03:58:54 +0500

    s

--- src/features/basket/components/widget/BasketWidget/BasketWidget.tsx
+++ src/features/basket/components/widget/BasketWidget/BasketWidget.tsx
@@ -1,12 +1,25 @@
 'use client';
 import styles from './BasketWidget.module.scss';
+import plural from '~/utils/plural';
 
-export function BasketWidget() {
+interface BasketWidgetProps {
+  count: number;
+}
+
+const pluralDic: Record<string, string> = {
+  one: 'астероид',
+  few: 'астероида',
+  many: 'астероидов',
+};
+
+export function BasketWidget({ count }: BasketWidgetProps) {
   return (
     <div className={styles.BasketWidget}>
       <div>
         <div className={styles.title}>Корзина</div>
-        <div className={styles.asteroidCount}>2 астероида</div>
+        <div className={styles.asteroidCount}>
+          {count} {plural(count, pluralDic)}
+        </div>
       </div>
       <button className={styles.sendButton}>Отправить</button>
     </div>
--- src/features/basket/containers/BasketWidgetContainer/BasketWidgetContainer.tsx
+++ src/features/basket/containers/BasketWidgetContainer/BasketWidgetContainer.tsx
@@ -1,5 +1,12 @@
+'use client';
+import { useContext } from 'react';
 import { BasketWidget } from '../../components/widget/BasketWidget';
+import { asteroidContext } from '~/features/contexts/AsteroidContext';
 
 export function BasketWidgetContainer() {
-  return <BasketWidget />;
+  const asteroidCtx = useContext(asteroidContext);
+
+  const asteroidCount = Object.keys(asteroidCtx?.state.basket ?? {}).length;
+
+  return <BasketWidget count={asteroidCount} />;
 }
